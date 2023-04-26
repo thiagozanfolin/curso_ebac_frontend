95c237eb9fb24cad862dba94cfc861914ea72836
thiagozanfolin <thiagozanfolin@hotmail.com>
2023-04-26 15:34:32 -0300

    Fisrt commit

--- script.js
+++ script.js
@@ -1,55 +1,5 @@
-$(document).ready(function() {
-    $('#carousel-imagens').slick({
-        autoplay: true,
-    });
+$("#cpf").mask("000.000.000-00")
 
-    $('.menu-hamburguer').click(function() {
-        $('nav').slideToggle();
-    })
+$("#cep").mask("00000-000")
 
-    $('#telefone').mask('(00) 00000-0000')
-
-    $('form').validate({
-        rules: {
-            nome: {
-                required: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            telefone: {
-                required: true
-            },
-            mensagem: {
-                required: true,
-            },
-            veiculoDeInteresse: {
-                required: false,
-            }
-        },
-        messages: {
-            nome: 'Por favor, insira o seu nome'
-        },
-        submitHandler: function(form) {
-            console.log(form)
-        },
-        invalidHandler: function(evento, validador) {
-            let camposIncorretos = validador.numberOfInvalids();
-            if (camposIncorretos) {
-                alert(`Existem ${camposIncorretos} campos incorretos`)
-            }
-        }
-    })
-
-    $('.lista-veiculos button').click(function() {
-        const destino = $('#contato');
-        const nomeVeiculo = $(this).parent().find('h3').text();
-
-        $('#veiculo-interesse').val(nomeVeiculo);
-
-        $('html').animate({
-            scrollTop: destino.offset().top
-        }, 1000)
-    })
-})
+$("#telefone").mask("(00) 00000-0000")
